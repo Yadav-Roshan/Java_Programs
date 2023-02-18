964f56de9d6d2a47d4c21890cf24b7ed8cdb2538
Roshan Yadav <31336534+Yadav-Roshan@users.noreply.github.com>
2023-02-18 16:53:33 +0545

    Update UserInput.java

---
+++ Assignment1-Calculator/UserInput.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class UserInput{
+	Scanner scan = new Scanner(System.in);
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	double[] numbers = new double[2];
+	// Using scanner class to input two numbers from the user
+	public double[] inputTwoNumbers(){
+		System.out.println("Enter first number: ");
+		numbers[0] = scan.nextDouble();
+		System.out.println("Enter second number: ");
+		numbers[1] = scan.nextDouble();
+		return numbers;
+	}
+	
+	//Using bufferred reader class to input an array of nubmers fromthe user
+	public double[] inputMultipleNumbers() throws IOException{
+		System.out.println("Enter Size of Array: ");
+		int size = scan.nextInt();
+
+		double array[] = new double[size];
+		String str = "";
+		try {
+			str = br.readLine();
+		} catch(IOException e){
+			System.out.println();
+		}
+		String[] array_numbers = str.trim().split("\\s+");
+
+		for(int i=0;i<size;i++) {
+			array[i] = Double.parseDouble(array_numbers[i]);
+		}
+		return array;
+	}
+}
