8bb2b696c6b916245bff07a1b6328db68c311882
Roshan Yadav <31336534+Yadav-Roshan@users.noreply.github.com>
2023-02-18 16:56:01 +0545

    Update Calculator.java

--- Assignment1-Calculator/Calculator.java
+++ Assignment1-Calculator/Calculator.java
@@ -1,49 +1,151 @@
-import java.lang.Math;
-
+//Only public class Calculator for performing given operations in dashboard
 public class Calculator{
-	static double[] result = new double[1];
-	// This is the function for adding two numbers
-	public double[] addNum(double[] numbers){
-		result[0] = numbers[0] + numbers[1];
-		return result;
-	}
-	//This is the function for subtracting two numbers
-	public double[] subtractNum(double[] numbers){
-		result[0] = numbers[0] - numbers[1];
-		return result;
-	}
-	//This is the fucntion for multiplying two numbers
-	public double[] multiplyNum(double[] numbers){
-		result[0] = numbers[0] * numbers[1];
-		return result;
-	}
-	//This is the function for dividing two numbers
-	public double[] divideNum(double[] numbers){
-		result[0] = numbers[0] / numbers[1];
-		return result;
-	}
-	//This is the function for adding numbers in an array
-	public double[] addArr(double[] numbers){
-		for(int i=0;i<numbers.length;i++){
-			result[0] += numbers[i];
-		}
-		return result;
-	}
-	//This is th function for finding varaince of the numbers in the array
-	public double[] varArr(double[] numbers){
-		double mean = addArr(numbers)[0]/numbers.length;
-		double var = 0;
-		//Summation of (x-x(mean))
-		for(int i=0;i<numbers.length;i++){
-			var += Math.pow(numbers[i]-mean,2);
-		}
-		result[0] = var/numbers.length;
-		return result;
-	}
-	//This is the function for finding standard deviation of the numbers in the array
-	public double[] stdArr(double[] numbers){
-		//Standard Deviation = 	√(Variance)
-		result[0] = Math.pow(varArr(numbers)[0],0.5);
-		return result;
-	}
+
+    //Method to calculate addition of given numbers
+    public double[] performAddition(double[] numbers){
+
+        //Declaring result array to store sum
+        double[] result = new double[2];
+
+        //Calculation
+        result[0] = numbers[0] + numbers[1];
+        
+        //returning result array with sum store at index 0
+        return result;
+    }
+    
+    //Method to perform subtraction of two numbers
+    public double[] performSubtraction(double[] numbers){
+
+        //Declaring result array to store difference
+        double[] result = new double[2];
+
+        //Calculation
+        result[0] = numbers[0] - numbers[1];
+        
+        //Returning result array with difference stored at index 0
+        return result;
+    }
+    
+    //Method to perform Multiplication on given two numbers
+    public double[] performMultiplication(double[] numbers){
+        
+        // Declaring result array to store product
+        double[] result = new double[2];
+
+        //Calculation
+        result[0] = numbers[0] * numbers[1];
+        
+        //Returning result array with result at index 0
+        return result;
+    }
+
+    //Method to perform Division on given two numbers
+    public double[] performDivision(double[] numbers){
+
+        // Declaring result array to store product
+        double[] result = new double[2];
+        
+        //Calculation
+        result[0] = numbers[0]/numbers[1];
+        
+        //Returning result array with result at index 0
+        return result;
+    }
+
+    //Method to perform Addiiton on given array of numbers
+    public double[] performArrayAddition(double[] numbers){
+
+        // Declaring result array to store Sum
+        double[] result = new double[4];
+
+        //Variable to store intermediate sum
+        double sum = 0;
+        
+        //Loop to add each elements of array numbers
+        for(int i =0; i<numbers.length; i++){
+            sum+= numbers[i];
+        }
+        
+        //Storing sum in result array
+        result[0] = sum;
+        
+        //Returning result array with result at index 0
+        return result;
+    }
+
+    //Method to find Variance of given array of numbers
+    public double[] performArrayVariance(double[] numbers){
+
+        // Declaring result array to store variance
+        double[] result = new double[4];
+
+        //Variable to store intermediate sum
+        double sum = 0;
+
+        //Variable to store intermediate sum of differnce of mean and each numbers in array
+        double sumdiff=0;
+        
+        //Loop to add each elements of array numbers
+        for(int i =0; i<numbers.length; i++){
+            sum+= numbers[i];
+        }
+
+        //Calculating mean
+        double mean = (double)sum/(double)numbers.length;
+        
+        //Loop to add each difference of mean and numbers of array
+        for(int j=0; j<numbers.length; j++){
+            sumdiff += (double)Math.pow(mean - numbers[j], 2);
+        }
+
+        //Calculating Variance
+        double variance = (double)sumdiff/(double)numbers.length;
+
+        //Storing variance in result array
+        result[0] = variance;
+
+        //Returnin result array
+        return result;
+    }
+
+    //Method to find Variance of given array of numbers
+    public double[] performArrayStd(double[] numbers){
+
+        // Declaring result array to store variance
+        double[] result = new double[4];
+
+        //Variable to store intermediate sum
+        double sum = 0;
+
+        double mean;
+        double sumdiff=0;
+        double variance;
+        double std;
+        
+        //Loop to sum each elements of array
+        for(int i =0; i<numbers.length; i++){
+            sum+= numbers[i];
+        }
+
+        //Calculating mean
+        mean = (double)sum/(double)numbers.length;
+        
+        //Loop to sum difference of mean and each numbers in array
+        for(int j=0; j<numbers.length; j++){
+            sumdiff += (double)Math.pow(mean - numbers[j], 2);
+        }
+
+        //Calculating variance
+        variance = (double)sumdiff/(double)numbers.length;
+
+        //Calculating standard deviation
+        std = Math.sqrt(variance);
+
+        //Storing standard deviation in result array at index 0
+        result[0] = std;
+
+        //Returnin result array with std deviation at index 0
+        return result;
+    }
 }
